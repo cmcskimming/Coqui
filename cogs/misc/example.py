9f650e29e8ed7e712ec12edcc82c096939f8b686
funy0n <cmcskimming03@gmail.com>
2024-01-07 04:21:07 -0800

    set up basic cog functionality and an example cog

---
+++ cogs/misc/example.py
@@ -0,0 +1,17 @@
+from discord.ext import commands
+import discord
+
+# pretty much just make a folder in the "cogs" folder to hold all the cogs for that category
+# just copy this template and replace "example" with the name of your cog
+# never put a naked file in the "cogs" folder always put it in a folder like how this one's in "misc"
+
+class example(commands.Cog):
+    def __init__(self, bot):
+        self.bot: commands.Bot = bot
+    
+    @commands.command()
+    async def test(self, ctx):
+        await ctx.send("hi")
+
+def setup(bot):
+    bot.add_cog(example(bot))
--- main.py
+++ main.py
@@ -1,9 +1,32 @@
+import os
 import discord
+from discord.ext import commands
 import json
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'logged in as {self.user}.')
+prefix = commands.when_mentioned_or('!')
+intents = discord.Intents.all()
 
-client = MyClient()
-client.run(json.load(open('token.json'))['token'])
+bot = commands.Bot(
+    command_prefix = '.', intents=intents
+)
+
+@bot.event
+async def on_ready():
+    print(f'logged in as {bot.user}')
+
+
+# so that we don't have to manually type all the cogs out to add them
+cogFiles = []  
+
+for folder in os.listdir("./cogs/"):
+    for filename in os.listdir(f"./cogs/{folder}/"):
+        if filename.endswith('.py'):
+            cogFiles.append(f"cogs.{folder}.{filename[:-3]}")
+
+for cogFile in cogFiles:
+    try:
+        bot.load_extension(cogFile)
+    except Exception as error:
+        print(error)
+
+bot.run(json.load(open('token.json'))['token'])
